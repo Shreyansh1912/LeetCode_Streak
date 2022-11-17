46f4069ae209fbe147686374c9d31cc73578c7ed
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-11-17 22:52:43 +0530

    Create Rectangle Area.java

---
+++ src/Rectangle Area.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        long area1 = ((long) C- (long) A) * ((long) D - (long) B);
+        long area2 = ((long) G- (long) E) * ((long) H - (long) F);
+        
+        //0 and Math.max in outer case is used for if rectangles are not overalpped i.e overlapped area is zero
+        long overlapped = Math.max((long) Math.min(C, G) - (long) Math.max(A, E), 0L) * 
+            Math.max((long) Math.min(D, H) - (long) Math.max(B,F), 0L);
+        
+        return (int) (area1 + area2 - overlapped);
+    }
+}
