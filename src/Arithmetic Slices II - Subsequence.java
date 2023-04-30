5ec4e424ede031a88689e9671057f4281765c9c2
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2023-04-30 21:56:45 +0530

    Update Arithmetic Slices II - Subsequence.java

--- src/Arithmetic Slices II - Subsequence.java
+++ src/Arithmetic Slices II - Subsequence.java
@@ -1,11 +1,10 @@
 class Solution {
       public int numberOfArithmeticSlices(int[] A) {
-      int res = 0;
+      int counts = 0;
       Map<Integer, Integer>[] map = new Map[A.length];
 		
       for (int i = 0; i < A.length; i++) {
         map[i] = new HashMap<>(i);
-        	
         for (int j = 0; j < i; j++) {
             long diff = (long)A[i] - A[j];
             if (diff <= Integer.MIN_VALUE || diff > Integer.MAX_VALUE) continue;
@@ -13,11 +12,12 @@ class Solution {
             int d = (int)diff;
             int c1 = map[i].getOrDefault(d, 0);
             int c2 = map[j].getOrDefault(d, 0);
-            res+=c2;
+            counts+=c2;
             map[i].put(d, c1 + c2 + 1);
         }
     }
-   return res;
+
+   return counts;
   }
 }
 		
